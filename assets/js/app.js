4a3511c536fbaeed2084c2cbba5886c8af8805cf
Spurtdigital <info@spurt.digital>
2026-06-14 20:20:26 +0200

    Chnga

--- assets/js/app.js
+++ assets/js/app.js
@@ -41,6 +41,79 @@ $(document).on("click", ".js-footer-toggle", function (e) {
     $nav.stop(true, true).slideToggle(220);
 });
 
+const initPartnerMarquee = function () {
+    const $sliders = $(".js-partner-marquee");
+
+    if (!$sliders.length) {
+        return;
+    }
+
+    $sliders.each(function () {
+        const $slider = $(this);
+        const $slides = $slider.children(".partner__slide");
+
+        // Ensure enough items for a seamless ticker loop on wide screens.
+        if ($slides.length > 0 && $slides.length < 12) {
+            const repeatCount = Math.ceil((12 - $slides.length) / $slides.length);
+
+            for (let i = 0; i < repeatCount; i += 1) {
+                $slides.clone().appendTo($slider);
+            }
+        }
+
+        if ($slider.hasClass("slick-initialized")) {
+            return;
+        }
+
+        $slider.slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            variableWidth: true,
+            arrows: false,
+            dots: false,
+            infinite: true,
+            rows:0,
+            autoplay: true,
+            autoplaySpeed: 0,
+            speed: 9000,
+            cssEase: "linear",
+            pauseOnHover: false,
+            pauseOnFocus: false,
+            swipe: false,
+            draggable: false,
+            touchMove: false,
+            rtl: false,
+            waitForAnimate: false,
+            responsive: [
+                {
+                    breakpoint: mq.xl,
+                    settings: {
+                        slidesToShow: 1
+                    }
+                },
+                {
+                    breakpoint: mq.lg,
+                    settings: {
+                        slidesToShow: 1
+                    }
+                },
+                {
+                    breakpoint: mq.md,
+                    settings: {
+                        slidesToShow: 1
+                    }
+                },
+                {
+                    breakpoint: mq.sm,
+                    settings: {
+                        slidesToShow: 1
+                    }
+                }
+            ]
+        });
+    });
+};
+
 var replaceMenu = function () {
     if ($(window).width() < mq.xl) {
         $(".js-panel").append($(".js-main-menu"));
@@ -53,6 +126,7 @@ var replaceMenu = function () {
 };
 
 replaceMenu();
+initPartnerMarquee();
 
 $(window).on("resize", function () {
     replaceMenu();
--- page-home.php
+++ page-home.php
@@ -20,6 +20,135 @@ get_header();
 	</div>
 </section>
 
+<section>
+	<div class="container mb-lg-5">
+		<div class="row align-items-end">
+			<div class="col-lg-6">
+				<h2 class="display-2">Alles onder 1 dak</h2>
+			</div>
+			<div class="col-lg-5 offset-lg-1">
+				<p class="lead mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec suscipit auctor dui, at convallis nisl. Donec in semper nunc. Donec suscipit auctor dui, at conv</p>
+			</div>
+		</div>
+		<!-- ACF repeater -->
+		<div class="row g-3 mt-lg-6">
+			<div class="col-12">
+				<div class="block-service-featured d-flex align-items-stretch h-100">
+					<div class="block-service-featured__content">
+						<h3 class="display-3 mb-2 fw-semibold">Maatwerk websites</h3>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, provident, vel unde veritatis delectus non assumenda eum itaque commodi qui aliquam similique, rem perferendis alias harum iusto. Natus, aliquam totam.</p>
+						<a href="#" class="block-service__link">Meer hierover <i class="fa-sharp fa-light fa-arrow-right ms-1 small"></i></a>
+					</div>
+					<div class="block-service-featured__media position-relative overflow-hidden">
+						<img class="img-abs-center" src="https://placeholdit.com/600x400/dddddd/999999" alt="">
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-4">
+				<div class="block-service">
+					<div class="block-service__media">
+						<img src="" alt="">
+					</div>
+					<div class="block-service__content">
+						<h3 class="display-5 fw-semibold">Webshops</h3>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.  Autem vel quibusdam iure sint necessitatibus praesentium explicabo mollitia </p>
+
+						<ul class="reset-list">
+							<li><a href="#">Shopify</a></li>
+							<li><a href="#">Shopify</a></li>
+						</ul>
+						<a href="#" class="block-service__link">Meer hierover <i class="fa-sharp fa-light fa-arrow-right ms-1 small"></i></a>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-4">
+				<div class="block-service">
+					<div class="block-service__media">
+						<img src="" alt="">
+					</div>
+					<div class="block-service__content">
+						<h3 class="display-5 fw-semibold">Webshops</h3>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.  Autem vel quibusdam iure sint necessitatibus praesentium explicabo mollitia </p>
+
+						<ul class="reset-list">
+							<li><a href="#">Shopify</a></li>
+							<li><a href="#">Shopify</a></li>
+						</ul>
+						<a href="#" class="block-service__link">Meer hierover <i class="fa-sharp fa-light fa-arrow-right ms-1 small"></i></a>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-4">
+				<div class="block-service">
+					<div class="block-service__media">
+						<img src="" alt="">
+					</div>
+					<div class="block-service__content">
+						<h3 class="display-5 fw-semibold">Webshops</h3>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.  Autem vel quibusdam iure sint necessitatibus praesentium explicabo mollitia </p>
+
+						<ul class="reset-list">
+							<li><a href="#">Shopify</a></li>
+							<li><a href="#">Shopify</a></li>
+						</ul>
+						<a href="#" class="block-service__link">Meer hierover <i class="fa-sharp fa-light fa-arrow-right ms-1 small"></i></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="home-partners bg-light py-xl-14">
+	<div class="container mb-lg-8">
+		<div class="col-xl-6 mx-auto text-center">
+			<h4 class="display-3">
+				Wij werken samen met ruim <strong>100</strong> partners
+			</h4>
+		</div>
+	</div>
+	<div class="partner-slide-one js-partner-marquee js-partner-marquee--left">
+		<div class="partner__slide">
+			<img src="https://t4.ftcdn.net/jpg/06/96/89/13/360_F_696891328_utj80ZwXsdy8SloC9IBaFGDIcGNBrEze.jpg" alt="">
+		</div>
+		<div class="partner__slide">
+			<img src="https://t4.ftcdn.net/jpg/06/96/89/13/360_F_696891328_utj80ZwXsdy8SloC9IBaFGDIcGNBrEze.jpg" alt="">
+		</div>
+		<div class="partner__slide">
+			<img src="https://t4.ftcdn.net/jpg/06/96/89/13/360_F_696891328_utj80ZwXsdy8SloC9IBaFGDIcGNBrEze.jpg" alt="">
+		</div>
+		<div class="partner__slide">
+			<img src="https://t4.ftcdn.net/jpg/06/96/89/13/360_F_696891328_utj80ZwXsdy8SloC9IBaFGDIcGNBrEze.jpg" alt="">
+		</div>
+		<div class="partner__slide">
+			<img src="https://t4.ftcdn.net/jpg/06/96/89/13/360_F_696891328_utj80ZwXsdy8SloC9IBaFGDIcGNBrEze.jpg" alt="">
+		</div>
+		<div class="partner__slide">
+			<img src="https://t4.ftcdn.net/jpg/06/96/89/13/360_F_696891328_utj80ZwXsdy8SloC9IBaFGDIcGNBrEze.jpg" alt="">
+		</div>
+	</div>
+	<div class="partner-slide-one js-partner-marquee js-partner-marquee--right">
+		<div class="partner__slide">
+			<img src="https://t4.ftcdn.net/jpg/06/96/89/13/360_F_696891328_utj80ZwXsdy8SloC9IBaFGDIcGNBrEze.jpg" alt="">
+		</div>
+		<div class="partner__slide">
+			<img src="https://t4.ftcdn.net/jpg/06/96/89/13/360_F_696891328_utj80ZwXsdy8SloC9IBaFGDIcGNBrEze.jpg" alt="">
+		</div>
+		<div class="partner__slide">
+			<img src="https://t4.ftcdn.net/jpg/06/96/89/13/360_F_696891328_utj80ZwXsdy8SloC9IBaFGDIcGNBrEze.jpg" alt="">
+		</div>
+		<div class="partner__slide">
+			<img src="https://t4.ftcdn.net/jpg/06/96/89/13/360_F_696891328_utj80ZwXsdy8SloC9IBaFGDIcGNBrEze.jpg" alt="">
+		</div>
+		<div class="partner__slide">
+			<img src="https://t4.ftcdn.net/jpg/06/96/89/13/360_F_696891328_utj80ZwXsdy8SloC9IBaFGDIcGNBrEze.jpg" alt="">
+		</div>
+		<div class="partner__slide">
+			<img src="https://t4.ftcdn.net/jpg/06/96/89/13/360_F_696891328_utj80ZwXsdy8SloC9IBaFGDIcGNBrEze.jpg" alt="">
+		</div>
+	</div>
+</section>
+
 <section class="bg-dark py-lg-10 py-5">
 	<div class="container">
 		<div class="row align-items-end">
